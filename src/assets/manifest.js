79ec40d004d779db487e4c39df88b8bc6878eadd
aurelienLRY <leroyaurelien11@gmail.com>
2024-04-04 16:20:33 +0200

    ajout de la librairy

---
+++ src/assets/manifest.js
@@ -0,0 +1,40 @@
+export const manifestForPlugIn = {
+    registerType:'prompt',
+    includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+    manifest:{
+      name:"Leroy Aurélien / Développeur web Frontend à Narbonne",
+      short_name:"Leroy Aurélien",
+      description:"I am a simple vite app",
+      icons:[{
+        src: '/img/favicon_io/android-chrome-192x192.png',
+        sizes:'192x192',
+        type:'image/png',
+        purpose:'favicon'
+      },
+      {
+        src:'/img/favicon_io/android-chrome-512x512.png',
+        sizes:'512x512',
+        type:'image/png',
+        purpose:'favicon'
+      },
+      {
+        src: '/img/favicon_io/apple-touch-icon.png',
+        sizes:'180x180',
+        type:'image/png',
+        purpose:'apple touch icon',
+      },
+      {
+        src: '/img/favicon_io/maskable_icon.png',
+        sizes:'512x512',
+        type:'image/png',
+        purpose:'any maskable',
+      }
+    ],
+    theme_color:'#171717',
+    background_color:'#f0e7db',
+    display:"standalone",
+    scope:'/',
+    start_url:"/",
+    orientation:'portrait'
+    }
+  }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugIn } from './src/assets/manifest.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   test: {
     globals: true,
     environment: 'jsdom',
