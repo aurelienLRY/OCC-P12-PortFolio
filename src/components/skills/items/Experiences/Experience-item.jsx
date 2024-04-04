98569b9881efdd0cb01242bd5317e920955e3d1e
aurelienLRY <leroyaurelien11@gmail.com>
2024-04-04 12:40:22 +0200

    modification du contenu

--- src/components/skills/items/Experiences/Experience-item.jsx
+++ src/components/skills/items/Experiences/Experience-item.jsx
@@ -3,7 +3,7 @@ export const Gerant = () => (
     <h3 className="experience-item_title">Cofondateur - Gérant SCOP</h3>
     <h4 className="experience-item_subTitle">Solution de réalité virtuelle</h4>
     <p className="experience-item_content">
-      Dirigé une coopérative spécialisée en réalité virtuelle, développant des
+      Diriger une coopérative spécialisée en réalité virtuelle, développant des
       compétences en gestion de projet et en développement d'applications
       innovantes.
     </p>
@@ -17,8 +17,7 @@ export const ExploitantDrone = () => (
       Spécialiste des prises de vues aériennes
     </h4>
     <p className="experience-item_content">
-      Gestionnaire de flotte de drones, assurant la conformité du matériel et la
-      sécurité des missions. Prototypage de drone, mise en conformité.
+      Gestionnaire de flotte de drones, assurant la conformité du matériel et la sécurité des missions. Prototypage de drone, mise en conformité.
     </p>
     <p className="experience-item_hashtag">
       #Captation #GestionDeProjet #InnovationTechnique
@@ -30,7 +29,7 @@ export const DroneLent = () => (
     <h3 className="experience-item_title">Chef d’équipe drone lent</h3>
     <h4 className="experience-item_subTitle">Renseignement militaire</h4>
     <p>
-      Responsable d’une équipe dans le renseignement d’origine image. Exerçant des posts tels que pilote drone, analyste image et chef de groupe.  
+      Responsable d’une équipe dans le renseignement d’origine image, occupant des fonctions variées telles que pilote de drone et analyste d'images.   
     </p>
     <p className="experience-item_hashtag">
     #AnalyseDeDonnées #GestionOpérationnelle #Leadership
